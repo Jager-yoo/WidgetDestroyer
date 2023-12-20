2d17c5be643121a5b4fb29a015f62b701b02d146
Jaeho Yoo <y73447jh@gmail.com>
2023-12-21 00:54:41 +0900

    Initial Commit

---
+++ WidgetDestroyer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WidgetDestroyer
+//
+//  Created by 유재호 on 12/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WidgetDestroyer/WidgetDestroyerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetDestroyerApp.swift
+//  WidgetDestroyer
+//
+//  Created by 유재호 on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct WidgetDestroyerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
